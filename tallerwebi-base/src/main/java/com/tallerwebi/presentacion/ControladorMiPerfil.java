35950cb612b5744bb433a0bfa4e9ee59aa7d43f2
dbcalaz <dbcalaz@gmail.com>
2025-10-10 16:02:46 -0300

    Agregando funcionalidad a la vista miPerfil.html.

--- tallerwebi-base/src/main/java/com/tallerwebi/presentacion/ControladorMiPerfil.java
+++ tallerwebi-base/src/main/java/com/tallerwebi/presentacion/ControladorMiPerfil.java
@@ -1,42 +1,40 @@
 package com.tallerwebi.presentacion;
 
-
-import com.tallerwebi.dominio.ServicioLogin;
+import com.tallerwebi.dominio.ServicioGenero;
+import com.tallerwebi.dominio.ServicioUsuario;
 import com.tallerwebi.dominio.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 @Controller
 public class ControladorMiPerfil {
 
-    private final ServicioLogin servicioLogin;
+    private final ServicioUsuario servicioUsuario;
+    private final ServicioGenero servicioGenero;
 
     // Inyección del repositorio a través del constructor
     @Autowired
-    public ControladorMiPerfil(ServicioLogin servicioLogin) {
-        this.servicioLogin = servicioLogin;
+    public ControladorMiPerfil(ServicioUsuario servicioUsuario, ServicioGenero servicioGenero) {
+        this.servicioUsuario = servicioUsuario;
+        this.servicioGenero = servicioGenero;
     }
 
-
-
     @GetMapping("/miPerfil")
-    @Transactional
+    @Transactional(readOnly = true)
     public ModelAndView miPerfil(HttpServletRequest request) {
         DatosUsuario datosUsuario = (DatosUsuario) request.getSession().getAttribute("usuarioLogueado");
-        System.out.println("Usuario en sesión: " + datosUsuario);
-        System.out.println("Email en sesión: " + datosUsuario.getEmail());
 
+        Usuario usuario = servicioUsuario.buscarPorEmail(datosUsuario.getEmail());
         ModelMap model = new ModelMap();
-        model.addAttribute("usuario", datosUsuario);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("generos", servicioGenero.listarGeneros());
 
         return new ModelAndView("miPerfil", model);
     }
-
-
 }
